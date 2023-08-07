b4697c244a21be0d6add0ab625bac8ede5d89eca
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-08-07 15:43:12 +0530

    Everything related  to format

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-
-import { airbnb, apple } from "../assets copy/index";
+import { apple } from "../assets copy/index";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
@@ -25,6 +24,7 @@ const Demo = () => {
     }
   }, []);
 
+  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -34,25 +34,31 @@ const Demo = () => {
 
     if (existingArticle) return setArticle(existingArticle);
 
-    const { data } = await getSummary({ articleUrl: article.url });
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles];
+    try {
+      const { data } = await getSummary({ articleUrl: article.url });
 
-      // update state and local storage
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+      if (data?.summary) {
+        const newArticle = { ...article, summary: data.summary };
+        const updatedAllArticles = [newArticle, ...allArticles];
+
+        // Update state and local storage
+        setArticle(newArticle);
+        setAllArticles(updatedAllArticles);
+        localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  // copy the url and toggle the icon for user feedback
+  // Copy the URL and toggle the icon for user feedback
   const handleCopy = (copyUrl) => {
     setCopied(copyUrl);
     navigator.clipboard.writeText(copyUrl);
     setTimeout(() => setCopied(false), 3000);
   };
 
+  // Handle Enter key press event
   const handleKeyDown = (e) => {
     if (e.keyCode === 13) {
       handleSubmit(e);
@@ -60,55 +66,50 @@ const Demo = () => {
   };
 
   return (
-    <section className='mt-16 w-full max-w-xl'>
+    <section className="mt-16 w-full max-w-xl">
       {/* Search */}
-      <div className='flex flex-col w-full gap-2'>
+      <div className="flex flex-col w-full gap-2">
         <form
-          className='relative flex justify-center items-center'
+          className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
-          <i
-            className='fa-solid fa-copy text-gray-600 text-lg absolute left-0 my-0 ml-3 w-5'
-          />
-          <i className=""></i>
+          <i className="fa-solid fa-copy text-gray-600 text-lg absolute left-0 my-0 ml-3 w-5" />
+          <i className="" />
           <input
-            type='url'
-            placeholder='Paste the article link'
+            type="url"
+            placeholder="Paste the article link"
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
             onKeyDown={handleKeyDown}
             required
-            className='url_input peer'
+            className="url_input peer"
           />
           <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
           >
             <p>↵</p>
           </button>
         </form>
 
         {/* Browse History */}
-        <div className='flex flex-col gap-2 my-3 max-h-60 overflow-y-auto'>
+        <div className="flex flex-col gap-2 my-3 max-h-60 overflow-y-auto">
           {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
-              className='link_card'
+              className="link_card"
             >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <div className="px-5">
-                  {
-                    (copied === item.url)
-                      ?
-                      <i class="fa-regular fa-circle-check text-gray-600 -ml-2" />
-                      :
-                      <i
-                        className='fa-solid fa-copy text-gray-600 -ml-2 ' />
-                  }
+                  {copied === item.url ? (
+                    <i className="fa-regular fa-circle-check text-gray-600 -ml-2" />
+                  ) : (
+                    <i className="fa-solid fa-copy text-gray-600 -ml-2" />
+                  )}
                 </div>
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                 {item.url}
               </p>
             </div>
@@ -117,25 +118,25 @@ const Demo = () => {
       </div>
 
       {/* Display Result */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
+      <div className="my-10 max-w-full flex justify-center items-center">
         {isFetching ? (
-          <img src={apple} alt='loader' className='w-20 h-20 object-contain' />
+          <img src={apple} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
-          <p className='font-inter font-bold text-white text-center'>
+          <p className="font-inter font-bold text-white text-center">
             Well, that wasn't supposed to happen...
             <br />
-            <span className='font-satoshi font-normal text-gray-300'>
+            <span className="font-satoshi font-normal text-gray-300">
               {error?.data?.error}
             </span>
           </p>
         ) : (
           article.summary && (
-            <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-200 text-2xl mb-2'>
-                Article <span className='blue_gradient'>Summary</span>
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-200 text-2xl mb-2">
+                Article <span className="blue_gradient">Summary</span>
               </h2>
-              <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-200 leading-6 tracking-wide'>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-200 leading-6 tracking-wide">
                   {article.summary}
                 </p>
               </div>
